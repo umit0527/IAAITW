9b5f660db79afdbca0ac150f0db9d8542145d39c
umit <umit0527@gmail.com>
2025-10-30 17:52:36 +0800

    update/finish calendar frontend

---
+++ IAAITW/IAAITW/Areas/Front/Controllers/CalendarController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAAITW.Areas.Front.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Front/Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
