3ff0b7cd84091c8a335c579590d8736748a09b2b
umit <umit0527@gmail.com>
2025-11-10 17:20:43 +0800

    update/finish about all frontend

---
+++ IAAITW/IAAITW/Areas/Front/Controllers/AboutController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IAAITW.Models;
+
+namespace IAAITW.Areas.Front.Controllers
+{
+    public class AboutController : Controller
+    {
+        private DBMdoelContext db = new DBMdoelContext();
+
+        // GET: Front/About/About
+        public ActionResult About()
+        {
+            var about = db.Abouts.Include(j => j.Admin).Include(j => j.UpdatedAdmin);
+            return View(about);
+        }
+
+        // GET: Front/About/Organization
+        public ActionResult Organization()
+        {
+            var organization = db.Organizationes.Include(j => j.Admin).Include(j => j.UpdatedAdmin);
+            return View(organization);
+        }
+
+        // GET: Front/About/History
+        public ActionResult History()
+        {
+            var history = db.Histories.Include(j => j.Admin).Include(j => j.UpdatedAdmin);
+            return View(history);
+        }
+
+        // GET: Front/About/Member
+        public ActionResult Member()
+        {
+            return View();
+        }
+
+        // GET: Front/About/Expert
+        public ActionResult Expert()
+        {
+            var expert = db.Experts.Include(j => j.Admin).Include(j => j.UpdatedAdmin);
+            return View(expert);
+        }
+    }
+}
---
+++ IAAITW/IAAITW/Areas/Front/Controllers/DeclareController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAAITW.Areas.Front.Controllers
+{
+    public class DeclareController : Controller
+    {
+        // GET: Front/Declare
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Front/Declare/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Front/Declare/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Front/Declare/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Front/Declare/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Front/Declare/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Front/Declare/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Front/Declare/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
