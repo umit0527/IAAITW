01b8222d20d071acc5e4fe8416b4877d1f635449
umit <umit0527@gmail.com>
2025-11-28 16:13:21 +0800

    update/finish front & back 404 + 500 + ServerError page

---
+++ IAAITW/IAAITW/Areas/Back/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAAITW.Areas.Back.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Back/Error404
+        public ActionResult Error404()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        // GET: Back/Error500
+        public ActionResult Error500()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+
+        // GET: Front/ServerError
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
---
+++ IAAITW/IAAITW/Areas/Front/Controllers/ErrorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IAAITW.Areas.Front.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Front/Error404
+        public ActionResult Error404()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        // GET: Front/Error500
+        public ActionResult Error500()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+
+        // GET: Front/ServerError
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
