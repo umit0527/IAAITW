2989d1e41b996a36d81b019d42f2970e6d0dd964
umit <umit0527@gmail.com>
2025-12-08 15:51:05 +0800

    update/adjust contact feild & add backend login + register icon

---
+++ IAAITW/IAAITW/Migrations/202512080721114_adjust feild in contact table.cs
@@ -0,0 +1,20 @@
+﻿namespace IAAITW.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adjustfeildincontacttable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Contacts", "Title", c => c.String(nullable: false, maxLength: 100));
+            AlterColumn("dbo.Contacts", "Content", c => c.String(nullable: false, maxLength: 500));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Contacts", "Content", c => c.String(maxLength: 500));
+            AlterColumn("dbo.Contacts", "Title", c => c.String(maxLength: 100));
+        }
+    }
+}
--- IAAITW/IAAITW/Models/Contact.cs
+++ IAAITW/IAAITW/Models/Contact.cs
@@ -11,31 +11,33 @@ namespace IAAITW.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "姓名必填")]
+        [Required(ErrorMessage = "請輸入姓名")]
         [Display(Name = "姓　名")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "性別必填")]
+        [Required(ErrorMessage = "請選擇性別")]
         [Display(Name = "性  別")]
         public string Gender { get; set; }
 
-        [Required(ErrorMessage = "聯絡電話必填")]
+        [Required(ErrorMessage = "請輸入聯絡電話")]
         [Display(Name = "聯絡電話")]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "E-mail必填")]
+        [Required(ErrorMessage = "請輸入E-mail")]
         [Display(Name = "E-mail")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "請輸入詢問標題")]
         [StringLength(100)]
         [Display(Name = "詢問標題")]
         public string Title { get; set; }
 
+        [Required(ErrorMessage = "請輸入詢問內容")]
         [StringLength(500)]
         [Display(Name = "詢問內容")]
         public string Content { get; set; }
 
-        [Display(Name = "建立時間")]
+        [Display(Name = "寄出時間")]
         public DateTime SentDate { get; set; } = DateTime.Now;
     }
 }
